2785bff23bdbb812000c0d966878ff7a72f4f176
Darshan Khairnar <darshan.a.khairnar@capgemini.com>
2023-07-02 11:39:53 +0530

    created layout of application

--- Book_Portal_UI/src/app/app-routing.module.ts
+++ Book_Portal_UI/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Auth/login/login.component';
+import { RegisterComponent } from './Auth/register/register.component';
+import { TitleComponent } from './Components/title/title.component';
+import { HomeComponent } from './Layout/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"home",component:HomeComponent,children: 
+  [
+    {path:"title",component:TitleComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Book_Portal_UI/src/app/app.module.ts
+++ Book_Portal_UI/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { RegisterComponent } from './Auth/register/register.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { SidebarComponent } from './Layout/sidebar/sidebar.component';
+import { HomeComponent } from './Layout/home/home.component';
+import { TitleComponent } from './Components/title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    HomeComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
